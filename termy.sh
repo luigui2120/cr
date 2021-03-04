8554bf452800399e6b7c8dddf15c714d2bb665ac
luigui2120 <78087869+luigui2120@users.noreply.github.com>
2021-03-04 16:47:42 -0500

    Add files via upload

--- termy.sh
+++ termy.sh
@@ -152,9 +152,21 @@ bannerExit(){
 }
 DIA=`date +"%d/%m/%Y"`
 HORA=`date +"%H:%M"`
-Scorpio28(){
-	sleep 0.5
-	clear
+credenciales(){
+clear
+negro="\e[1;30m"
+azul="\e[1;34m"
+verde="\e[1;32m"
+cian="\e[1;36m"
+rojo="\e[1;31m"
+purpura="\e[1;35m"
+amarillo="\e[1;33m"
+blanco="\e[1;37m"
+#credencial
+crede="luigui"
+#############
+sleep 0.5
+clear
 echo -e "${verde}
         ╔━━━━━━━━━━━━━━━━━━━━━━╗
         ┃   ${blanco}••   …………………   ●   ${verde}┃
@@ -183,30 +195,11 @@ echo -e "${verde}
            └══════════════════════┘
         ${verde}██████████${blanco}█████████████${rojo}██████████
     ${verde}      ┌═════════════════┐
-          █${amarillo}FECHA=${blanco}${DIA}${verde}█
+          █${amarillo}FECHA=${blanco}${DIA}${verde} █
           └═════════════════┘
-	┌═══════════┐
-        █${amarillo}HORA=${blanco}${HORA}${verde}█
-        └═══════════┘
-	    
-"${blanco}
-PS1="\[\e[1;32m\]╔════[\[\e[37m\]\T\[\e[1;32m\]]═══\e[1;32m[\e[37mScorpio28\e[37m\e[1;32m]═══[\[\e[31m\]\e[0;35m\W\[\e[1;32m]
-╚════<\e[0;35m$\e[1;32m>\e[0;32m "
-}
-credenciales(){
-clear
-negro="\e[1;30m"
-azul="\e[1;34m"
-verde="\e[1;32m"
-cian="\e[1;36m"
-rojo="\e[1;31m"
-purpura="\e[1;35m"
-amarillo="\e[1;33m"
-blanco="\e[1;37m"
-#credencial
-crede="luigui"
-#############
-Scorpio28
+	  ┌═══════════┐
+          █${amarillo}HORA=${blanco}${HORA}${verde} █
+          └═══════════┘" ${blanco}
 echo -e -n "${verde}
 ┌══════════════════════┐
 █${blanco}Introduzca credencial:${verde}█
@@ -355,12 +348,14 @@ clear
 while :
 do
 echo '	
-╔███╗  ╔███╗ ╔█████╗ ╔██████╗  ╔███████╗   
-║██║║██║║██║ ██╔══██╗║██║  ║██ ║██╔════
-║██║║██║║██║ ███████║║██║  ║██ ║███████║
-║██║╚══╝║██║ ██╔══██║║██║  ║██ ║██╔════  
-║██║    ║██║ ██║  ██║║██████║  ║███████║    
-╚══╝    ╚══╝ ╚═╝  ╚═╝╚══════╝  ╚═══════╝V2.3
+╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+┃ ╔███╗  ╔███╗ ╔█████╗ ╔██████╗  ╔███████╗     ┃
+┃ ║██║║██║║██║ ██╔══██╗║██║  ║██ ║██╔════      ┃
+┃ ║██║║██║║██║ ███████║║██║  ║██ ║███████║     ┃ 
+┃ ║██║╚══╝║██║ ██╔══██║║██║  ║██ ║██╔════      ┃
+┃ ║██║    ║██║ ██║  ██║║██████║  ║███████║     ┃
+┃ ╚══╝    ╚══╝ ╚═╝  ╚═╝╚══════╝  ╚═══════╝V2.3 ┃
+╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
 '| lolcat -a -d 20
 echo "Este script esta hecho con fines educativo"
 echo -e -n "${verde}
@@ -369,24 +364,27 @@ echo -e -n "${verde}
 └═══════════════┘
 "${blanco}
 color
-echo -e "$grn ＞>>＞$ylo [Options]$grn ＜<<<＜＜$rset"
-
-echo " "
-echo " "
-echo -e "$grn [1] $yellow Copiar el interfast de una pagina html"
-echo -e "$grn [2] $yellow Copiar una pagina completa de html vulnerando robot"
-echo -e "$grn [3] $yellow Copiar html aun con restricciones"
-echo -e "$grn [4] $yellow Copiar html con limite de velocidad para no ser detectado"
-echo -e "$grn [5] $yellow Verificar el texto o Html"
-echo -e "$grn [6] $yellow Ver linia de codigo"
-echo -e "$grn [7] $yellow Modificar Html o texto"
-echo -e "$grn [8] $yellow Crear texto o Html"
-echo -e "$grn [9] $yellow Simulador de pagina web"
-echo -e "$grn [10] $yellow Enviar al correo gmail"
-echo -e "$grn [11] $yellow Mas Opciones"
-echo -e "$grn [24] $yellow Exit"
-echo " "
-echo " "
+echo -e -n "${verde}
+      ┌══════════┐
+      █${blanco} OPTIONES${verde} █
+      └══════════┘
+"${blanco}
+echo -e -n "${yellow}
+╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+┃$grn [1] $yellow Copiar el interfast de una pagina html                    ┃
+┃$grn [2] $yellow Copiar una pagina completa de html vulnerando robot       ┃
+┃$grn [3] $yellow Copiar html aun con restricciones                         ┃ 
+┃$grn [4] $yellow Copiar html con limite de velocidad para no ser detectado ┃
+┃$grn [5] $yellow Verificar el texto o Html                                 ┃
+┃$grn [6] $yellow Ver linia de codigo                                       ┃
+┃$grn [7] $yellow Modificar Html o texto                                    ┃
+┃$grn [8] $yellow Crear texto o Html                                        ┃ 
+┃$grn [9] $yellow Simulador de pagina web                                   ┃ 
+┃$grn [10] $yellow Enviar al correo gmail                                   ┃
+┃$grn [11] $yellow Mas Opciones                                             ┃ 
+┃$grn [24] $yellow Exit                                                     ┃
+╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
+"${blanco}  
 
   echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa opcion:\e[0m""\e[1;31m]\e[0m"
   read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' x
@@ -649,195 +647,19 @@ done
 }
 EmailSpoofing
     ;;
-    11)
-clear
-echo '	
-╔███╗  ╔███╗ ╔█████╗ ╔██████╗  ╔███████╗   
-║██║║██║║██║ ██╔══██╗║██║  ║██ ║██╔════
-║██║║██║║██║ ███████║║██║  ║██ ║███████║
-║██║╚══╝║██║ ██╔══██║║██║  ║██ ║██╔════  
-║██║    ║██║ ██║  ██║║██████║  ║███████║    
-╚══╝    ╚══╝ ╚═╝  ╚═╝╚══════╝  ╚═══════╝V2.3
-'| lolcat -a -d 20
-echo -e -n "
-${blanco}Este script esta hecho con fines educativo 
-"${blanco}
-echo -e -n "${verde}
-┌═══════════════┐
-█${blanco}AUTOR LUIGUI${verde} █
-└═══════════════┘
-"${blanco}
-color
-echo -e "$grn ＞>>＞$ylo [Optiones]$grn ＜<<<＜＜$rset"
-echo " "
-echo " "    
-echo -e "$grn [12] $yellow Copiar el html o texto"
-echo -e "$grn [13] $yellow Mover el html o texto"
-echo -e "$grn [14] $yellow Copiar carpeta"
-echo -e "$grn [15] $yellow Mover carpeta"
-echo -e "$grn [16] $yellow Eliminar archivo o carpeta"
-echo -e "$grn [17] $yellow Descargar Imagenes"
-echo -e "$grn [18] $yellow Imprimir el codigo de la url en la terminal"
-echo -e "$grn [19] $yellow Sacar la información del encabezado de un sitio web HTTP"
-echo -e "$grn [20] $yellow Obtener más información del sitio web"
-echo -e "$grn [21] $yellow Almacenar cookies de sitio web"
-echo -e "$grn [22] $yellow Acceder a los archivos que han sido modificados después de un tiempo del html"
-echo -e "$grn [23] $yellow Retornar al inicio"
-echo " " 
-    	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa Opcion:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' vol
-         if [[ $vol -eq 12 ]]; then
-         echo -n "Usted guardo en carpeta (y/n)? "
-	read answer
-        if [ "$answer" != "${answer#[Yy]}" ] ;then
-	clear 
-         echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre de la carpeta:\e[0m""\e[1;31m]\e[0m"
-         read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var12
-         echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre del texto o html:\e[0m""\e[1;31m]\e[0m"
-         read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var13
-	 cp $my_var12/$my_var13 /sdcard/
-	 reset
-         else
-         echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre del texto o html:\e[0m""\e[1;31m]\e[0m"
-         read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var14
-	 cp $my_var14 /sdcard/
-	 reset
-         fi 
-	elif [[ $vol -eq 13 ]]; then
-	echo -n "Usted guardo en carpeta (y/n)? "
-	read answer33
-        if [ "$answer33" != "${answer33#[Yy]}" ] ;then
-	clear 
-         echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre de la carpeta:\e[0m""\e[1;31m]\e[0m"
-         read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var17
-         echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre del texto o html:\e[0m""\e[1;31m]\e[0m"
-         read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var16
-	 mv $my_var17/$my_var16 /sdcard/
-	 reset
-         else
-         echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre del texto o html:\e[0m""\e[1;31m]\e[0m"
-         read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var15
-	 mv $my_var15 /sdcard/
-         reset
-         fi
-         elif [[ $vol -eq 14 ]]; then
-         echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre de la carpeta:\e[0m""\e[1;31m]\e[0m"
-         read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var18
-		cp $my_var18 /sdcard/
-	 reset
-	 elif [[ $vol -eq 15 ]]; then
-	 echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre de la carpeta:\e[0m""\e[1;31m]\e[0m"
-         read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var19
-	 mv $my_var19 /sdcard/
-         reset
-        elif [[ $vol -eq 16 ]]; then
-        echo -e "$grn [1] $cyan Eliminar Archivo "f" "
-	echo -e "$grn [2] $cyan Eliminar carpeta "rf""
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el codigo a eliminar:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var20
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre de la carpeta:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var21
-	rm -$my_var20 $my_var21
-	clear
-	elif [[ $vol -eq 17 ]]; then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el url de la pagina:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var31
-	wget $my_var31
-	sleep 6
-	clear
-	elif [[ $vol -eq 18 ]]; then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el url de la pagina:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var22
-	curl $my_var22
-	sleep 6
-	clear
-	elif [[ $vol -eq 19 ]]; then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el url de la pagina:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var23
-        echo -n "Desea guardar registro en una carpeta(y/n)? "
-	read anseer
-	if [ "$anseer" != "${anseer#[Yy]}" ] ;then
-        echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre para el archivo:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var31
-	curl -I  $my_var23 | tee -a $my_var31.txt
-	clear
-	else
-	curl -I  $my_var23
-	sleep 8
-	clear
-	fi
-	elif [[ $vol -eq 20 ]]; then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el url de la pagina:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var24
-	curl -v $my_var24
-	sleep 3.5
-	clear
-	elif [[ $vol -eq 21 ]]; then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el url de la pagina:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var50
-        echo -n "Desea guardar registro en una carpeta(y/n)? "
-	read ansee
-	if [ "$ansee" != "${ansee#[Yy]}" ] ;then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre de la carpeta:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var32
-	mkdir $my_var32
-	curl --cookie-jar $my_var32/cookies.txt $my_var50/cookies.html -O
-	mv cookies.html $my_var32/
-	else
-	curl --cookie-jar cookies.txt $my_var50/cookies.html -O 
-	fi
-	sleep 3.5
-	clear
-	elif [[ $vol -eq 22 ]]; then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el url de la pagina:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var28
-        echo -n "Desea crear carpeta (y/n)? "
-	read anse
-	if [ "$anse" != "${anse#[Yy]}" ] ;then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre de la carpeta:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var31
-	mkdir $my_var31
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa la fecha d/m/a:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var29
-        echo -n "Desea guardar el registro (y/n)? "
-	read ans
-	if [ "$ans" != "${ans#[Yy]}" ] ;then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre del texto:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var30
-	curl -z $my_var29 $my_var28 | tee -a $my_var31/$my_var30.txt
-	sleep 2
-	clear
-	else
-        curl -z $my_var29 $my_var28
-        sleep 8
-        clear
-         fi
-	else
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa la fecha d/m/a:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var29
-        echo -n "Desea guardar el registro (y/n)? "
-	read ans
-	if [ "$ans" != "${ans#[Yy]}" ] ;then
-	echo -e "\e[1;31m┌─[\e[0m""\e[1;37mIngresa el nombre del texto:\e[0m""\e[1;31m]\e[0m"
-        read -p $'\e[1;31m└──╼\e[0m\e[1;92m ' my_var30
-	curl -z $my_var29 $my_var28 | tee -a $my_var30.txt
-	sleep 2
-	clear
-	else
-        curl -z $my_var29 $my_var28
-        sleep 8
-        clear
-        fi
-	fi
-	elif [[ $vol -eq 23 ]]; then
-	cd Html
-        ./retornal.sh
-	clear
-        fi
-        
+11)
+cd Html
+./retorna.sh
     ;;
-     
-   24s)
+     HELP|help)
+     echo "hola senores"
+     clear
+    ;;
+OPTIONS|options)
+     echo "hola senores"
+     clear
+    ;;
+   24)
 echo '
  ------------------------------------
 | NOS DA PENA QUE NOS DEJE CUIDATE   |     
